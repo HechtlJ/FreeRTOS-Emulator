3ea9e5c8d89ea7815e8a1db916b3f8f3c94ad0f0
Johannes Hechtl <johannes.hechtl@tum.de>
2020-06-16 17:05:36 +0200

    added player definition

---
+++ src/player.h
@@ -0,0 +1,15 @@
+
+
+typedef struct {
+    unsigned int Life;
+    unsigned int x_coord;
+} player_t;
+
+
+
+
+void paint_player(player_t *player);
+void player_move_right(player_t *player);
+void player_move_left(player_t *player);
+void player_shoot(player_t *player);
+void player_reduce_life(player_t *player);
