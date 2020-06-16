a2ea50ccdd1d73fff30dfd5330a2640237cfe2ce
Johannes Hechtl <johannes.hechtl@tum.de>
2020-06-16 18:16:43 +0200

    implemented moving for player

--- src/player.c
+++ src/player.c
@@ -5,6 +5,8 @@
 #define PLAYER_BODY_HEIGTH 25
 #define PLAYER_CANNON_WIDTH 8
 #define PLAYER_CANNON_HEIGTH 20
+#define PLAYER_MAX_X SCREEN_WIDTH - 10 - PLAYER_WIDTH
+#define PLAYER_MIN_X 10
 
 
 void paint_player(player_t *player){
@@ -13,7 +15,19 @@ void paint_player(player_t *player){
     tumDrawFilledBox(player->x_coord, PLAYER_Y_COORD, PLAYER_WIDTH, PLAYER_BODY_HEIGTH, Green);
 
     //draw Cannon
-    x_coord = player->x_coord + (int)((1.0/2.0) * (PLAYER_WIDTH - PLAYER_CANNON_WIDTH));
-    y_coord = PLAYER_Y_COORD - PLAYER_CANNON_HEIGTH;
+    int x_coord = player->x_coord + (int)((1.0/2.0) * (PLAYER_WIDTH - PLAYER_CANNON_WIDTH));
+    int y_coord = PLAYER_Y_COORD - PLAYER_CANNON_HEIGTH;
     tumDrawFilledBox(x_coord, y_coord, PLAYER_CANNON_WIDTH, PLAYER_CANNON_HEIGTH, Green);
+}
+
+void player_move_right(player_t *player){
+    if(player->x_coord < PLAYER_MAX_X){
+        player->x_coord++;
+    }
+}
+
+void player_move_left(player_t *player){
+    if(player->x_coord > PLAYER_MIN_X){
+        player->x_coord--;
+    }
 }
